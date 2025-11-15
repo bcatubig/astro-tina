5c56d50b98f221fa4eb24b3e02189bb5457aae2e
Brandon Catubig <bcatubig@pm.me>
2025-11-15 13:21:16 -0800

    wire in more tina

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import tinaDirective from "./astro-tina/register";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: process.env.SITE_URL || "https://astro-tina.bcatubig.workers.dev/",
+  integrations: [mdx(), sitemap(), tinaDirective()],
+});
